da7784c0de3f32eb1a5f23b893ad4b86072b7dc1
UEALTT <34135547+UEALTT@users.noreply.github.com>
2019-01-29 09:30:39 +0000

    Add files via upload

---
+++ data/quiz1.js
@@ -0,0 +1,39 @@
+(function(){var loadHandler=window['q_{B82B25C0-CA82-4054-98ED-F5BD5D16BB11}'];var quizInfo = "eNrtG2lz2sjyr2jZIvliMNjGB8lmnwBhawMSkUQcb5yiZBiM1kJSdJg4qfz31z0zujHGm2Tj2tocoKOv6e7pY2b4Ugkr7cp1ZadiwLfnW074MbI+w31QaX+pWPgx6ky6Z6qqSxNJ18WLiajo55I2GYgdaQBII5uYARGIExJfMB34F6zgKnSFcEGEjxEJQst1BMuh9yH5FApzi9gz4JFS7suDwURWJsaZNOkMROX1D+Fyphr6SDXuoe1FoWAKCzcMPDfM4Q3k15JmTPSuOJDuQZ4uXBe+7pUsR28oGt0zWTm9h9bSDKcLwbRtwQrJMhDmvrukxGwyD4Wpa0dLR1hZ4YI+nLq+T0BkZ2Y510UM37pexCjrRdDH2lvp4mdKMh4Y8ggUC/dy9/vot0DSkN6VjH7hRoJDyAwJBcQm05DJD9+cA/gSMWH4M9/1ajN35Qi2FYQPM1qv0TX80sE8hpUmgQ8r+gOKch347wtL1yecSbBRZcp4KGlyd/O0E5xoSXxruo0NYoIb/etv0B3J3dfgvcrF9x0/Jasq38X99DNVS/ztR8QxQxtDyBQH+ncR17gYSRh7f4Sk52dy92xyrmq9ByOMZ5tTsnDtGZgqTwOxO/fnhC1IXEj6RFG/TVkDSdTiDKJPOmPDUJWEVBdI+RxQVQxNHUxGoiIN6J2sgLmKCK4TWk5E1uFI72QDpvlbWTovomkkiOwwWIfVlxVZP5u8Gct/FrH6lmMFi3VImmSIr6W1SBoJzRsivGHlwBpMKt96zFuLrLKYY4heoiG/hbnRVTUpgTTc0LSFkQtlR9AWqiNVVgy9KriQid25UDVUQwR+7Ckj1QOB5YHUwzAIEVFPSPVAXMuG+JpTUU8WB+rppKO+m3RFpQtyM1nRAqYzJXYZLG+wkqkyoOBRCZDill6rr9PX6uvSa/21PEoBRm4QQuYsMzE0iHSnIszPBNbw7wTx2rScEjA1SayZBP4tWuM+teDUSCAvCAeQhiPjIokMfVka9CbnoqZAxZBOnJUPOV64cyOfzxyG25ekXkeEcKpJQ5Cb1jmGARR1ngYX5i0Rql11rBj8DRrdDKFg8GIJEyJjZSAPZQMsHoMClbFjW0tgzoPNqSb2AODNWNINGRwRfEyTuga1HU3p68FkJQWUnekm0JGoGaCzwUWG9sj0QwvCzp2QQ5WVt+JAhoilgcPREmuIdfOpROU2r2yCAcYxb61rMyRxsHEwfiZB9YpMzQg0bIXCygyc5yGE2akdzcC9aby1AgHL9DrjOAQlozvFIRwE51k3lh9Yi9RCscaKKLyA0WPPMGRjgALrSaUiZiycYnNwfdwBG6V+1Es8OQVNLMrVkfDoQ1V0ZU5vighKUqlnLEGnWijcN5ocTlkJm1ATWw9k3UiEe8NNEpTBIfDxIJgCW58FFv3WgmOs1fhsAicZZzUsvDXtqKS0eAYyPePcv/OIcIFTLmsQhdad4HWlPKOAXzGYEQqrjnW80CXFEOlg1yJ1EmskSGvhRj6M1Y2CGBbi9KQnGmjaIXyInUEa6nvg7Je7hrUkWeg+C+aoHfQEGsGz71NOkCLyMYwBjERdTwmMzCDIE8D3tM3J5R2Ewx4lgJmbk4fFzkK6zQLoxrgn4XeOnh7C3AQB9SK9sQ4Gl3s49QOoLORem73NO5us9FWu4Zii7MxdQbetGVmHEEPF3ilUlSrNmDSoVgsohbFjtmXelqbcAgbaTx0bLF5DQQRWEyDmRJ4wh6Kaxx/Guh6jon/fh1YXurY1vREuIRFeVmgLFF1BAGfJIxPKUP9DXo/oBrpSGjzP3cieIQY0Rzc0cEK3CY2DsFrAfGYvsC115/PfGTVw8x4aX5NGUJHTycZ6U+RIscGb6vV6Cs1BJ4Y6weQ14A6a+Cbr3pxZjgI+JEt4Xxd6LpVjZYKKqYChf/f7WvJjbXvimCDAfW6Jv5kDVhTrCjL9xvIy5Rj1NJ15kghZHTMd85GuOhgPabZYmf4snklZeFnpSe9SuF/LEEPxXZEaK/DKoGhhiC0JXHa6ZeFofE3B3uQKcyACGl077BAydHbcLEtBGi9BMm8UbbsEGc8Jnd511SH04FiNJFkMQi8qjHtgEPGL2EDc0bE9iftQE0w8pyU57zOsz79v4FvgZFBvN+E/KAGoBi5MgcgxeVpLJmZQF+4VyvPdKa5F4NINFttTd+nZJMSpgYImJDaJRVPtYKCe82KRpd2SuDqKB6Ep21EFAoqKZU0sdV2ImzEmCcnLgTJPeRlez8m01pAMAqNRpoRLy9e0BkWQKhKPO79iJZZS0jArjw0p7TiWVu61LkHr0EtfB2QKr99MxLFxpmrynyzf9lVtGK8WjTTwJtRRjRUBtfvgJU1TNRaQymsntyZkCRZ/BHM2g7ARbKbD6g9NgimjseiDbTsGap+AL7LSaC1+XOdIlDWN8aztYhx5aZx4QgzOi3BesTDYfraL47mvJ/VFvIvx0E4zGJsDVZtn8mGtXXJLK/+iZgVeXzHdxotSCXxHMs4lCYPKFQlXhEAmVRVwCZyiVeNcra5H68n9PowQ0FA20Bp0seBFFHc9BmhbHCDCNsCnmgSpT5sYZyKKdu0TM6TeaTrbok3QYWKeOQKQwLeRYQDTOBbABp96kHuCkGOdoq7ny+u3jX4ADfi1b4aRbfK4ho7h0VLvF0okh11qGdIpGZf+UPeA+NUybtL2QTLJZ8bMMq7PyrytiOBqApaLhQ6Bp7kNiJvr1s0CjCStK9FiNlvqA3qVNh3VdSi0DLwXQ/DiDF7A4y1WcZh6BMXK3b0DZes6Zfuw5aCk5coOkDN6KF48h6jOUiuWU1g2UUkoCVqL83Y3g5J0Ul93Kh9xvyvAWYo7YDeV9ty0A7JTESvt5k7Fo3tiXqV93ABYH3fOPA8AzTk+Jwk03L7/QjfWFJO2OxFmC/i+dbARQDEUcSgBQwYlYexeC8ZCfgzXhdxoOnccchZDYk0iKhcJWI94UPcsIT8UIXsSrmMMwVIJ8B/ulWBYoU2KsH+oHaakGHK0YE19Dmp0BjM5ARGT9JMDEns9sJte+foB1DZHwAWK9hE0GfoRgYcmKnDOFQj3M24HfA2GCBLVLk1qiSU8aTbqjR20Bn81qrTpk1n6aEYNM6WfyPalBzXTnU1+e47L1TVInNdOG3uGF89fvQxAt/HrqWu7fvvXBv3zYg6VRy2wPpN2s+V9Yrdzc2nZd+25EzYm+4d7+3s7qgfZQ4dCAogZCzN8Znpu8CJgG1+/0JiV1Gh8wci+q7+83EXGr+Dbe4UuaP3z0mbzLF8MT7bzIHWyPb2AlGX1foqs8WojLo1Bmbh0g5ApuSyg+VNM79wEtE/OrIwWRfuKAYQ6ZzIHPDN/P8/do2fwHflKhU6Q9UOa0rLwnkEd9PBvZlD7h/GgVgQ12L5y7VlxlM3iKKGmWloU8vmrc2JPscLnXSpbp9GhV7WmJKC9V37kEEtHOI5FMkmv+DCZnTB4TLGxnNM/FRy4m8RTmNtxrJl6LJ70/pYmSubdO9jOvGwdA4f67GPkhi/SHpPdC1dRGLpOpskqzxp/G0Nu8M0fYMZNJdYPNCC/tLgtgygJ+V58BYmZX838hGUwTV7P0ofz+KGX4uTnfsHeeWco+w59Yjm4tNduAPzzV194pfM1seb8CVqz2Dj9Z8FNFjS5Ad9/oKee8OuWfc3ZV58OHk2cBoX4hJSNm3uxDVjh876CKzrNy100U33lggp3co/CcF75QLXOx22llY+XXDKOb/pOc0tee2Veew/xYrkmYbXtsPbLrPYfyWp/S1YHZVYHj2TVvNqSV6vMq5XhxRxyswK3ZXVYZnX4OFb727I6KrM6egSrNNis5ZfEnTLj4zLj4wcYs8sPrBXjEWeJ62goyZeD1t5xf++oVTtoNDq1g/7JSU08OBRrrU6/L3b2D4+OT06+fq8w3Dz+1jAsh7iohs3s0gxpP2qGwgo/POJjgDKv6dKoR1zPJmxJkq7mQvG4xBezu8Amnyzz9x8XwClkk2r8J/RJrVqzUd1KBU+iUfoWcZ9+r5R4yLcLePgIfRZ9OykMPNrQx776jwnVbNX2ylL9JGH2GrX9e4WBMLrKlAv5WCn2u4eto8M+REgMmL2mVOscHki1rtQ/2WscS4fd7v4TipV96JfFxuWu2BSQouCBpxI/2GEHiGnQnFlBiGe2hGBBd4KhCbsTrojgE3OGx2r+tVGS75/U2Y4L00yshMz4mar2hCUJfRI8iYCZnKr6RtH/C55rQgPX1lOLn/v3yPWTxGltFmdNDA3ncQxtnfRbjdZho9bvNESoNxvHtU5jf6/WPNlvHDbEvdbhfuMJxdDzBTykjHGPiK4+XrmzO3o2GwJphEcV/4qgHJ1bhO734jLWFdSnguvRozz/1gDaR/HrwgBPB+UUgIN/EoHykRL+hHBjQEwpyvEPi0CVdE+4A9f6QCf1F9aj4gc6pJxo6jqe1Z3u0eGR1DmqNY8a/dpB5/io1jnqHNdaB6J43Djebx5IdFY72QN2p74beUKyFFMmHzyuSY2VFzyuXiuhbReiUDsg7RJFh3B3ctDYwWl90Go8PKnjrc6X2MVrxHP90CBLz8bDy7dQooF2frus7F1WhE9L2wngehGGXvty93J3tVrVrQB/VXgduHbElpan7vJyF2ldRRb+UoPdXO42LyuvXoaccpBeCtYMaEYBMcwbYEc3S3US4mGlAFF+ed/tiYb4nsaoDx/QMWLU7DVQXJIggJY7vWKksfbIErJ0KrGgxyILz+zwRRB5OPT/rRvPs+vwBePMCRc4BNHVX1AEZZm8ryZnHMd69QPdQYiP/LX53kKVH6o0BlKVPdnEBMrjqW1aS9Boho+BZzWtgP66JQrdpRlaU3pm/Zo4xKfLEuzoED1LiCPMH45k+5Y+lmpTYt2iXujxz/iEenyADakvoIZfQKkUeGTKAhjNQbmjSXRjLFh3DHPT2K59c0ZmqKPs2E7pU6q6zdrHIwBFZH5E4SFkGKNPT1tkcelZ3oCdwdiEvDQ/lXCH5idrGS23QffYaY8Sidzp5c0kZpFPd3Sy6NTV8GzuZlRmlywiO87xsMxkVpSWzDbhzOnp1ywOOw+7CYdV5mXV8J8SPYwq8p+bZLHjZ5vx0SWK3tB+CEGePQoFppAT4mF56IthzmZxq/qIHbXBo4vpr6TofYFkchngdTl+Qw7/P89iyhw=";var skinSettings = {
+    "appearance" : {
+        "hidePlayerFrame" : false,
+        "questionList" : {
+            "showScore" : true,
+            "showAwardedScore" : true,
+            "enabled" : true,
+            "showStatusIcon" : true
+        },
+        "hideSlideBackground" : false,
+        "scoreInfo" : {
+            "displayCumulativeScore" : true,
+            "displayQuestionScore" : true
+        }
+    },
+    "colors" : {
+        "feedback.title.text" : "#FFFFFF",
+        "radio.check.highlight" : "rgba(254, 215, 0, 0.3)",
+        "dialogBox.button.over" : "#D6D7D8",
+        "hotspot.content.normal" : "#2782DF",
+        "button.over" : "#F3F3F3",
+        "feedback.title.answered" : "#0094D4",
+        "dialogBox.button.content.normal" : "#545454",
+        "button.content.normal" : "#545356",
+        "feedback.title.correct" : "#46B03F",
+        "topPanel.text" : "#1A1A1A",
+        "buttons.menus.highlight" : "#FFF7CF",
+        "player.background" : "#D8D6DB",
+        "feedback.title.incorrect" : "#DB5A4B",
+        "button.content.over" : "#545454",
+        "dialogBox.button.content.over" : "#545454",
+        "radio.check.content.normal" : "#2E76BF",
+        "buttons.menus.highlight.second" : "#FFED85",
+        "feedback.title.partial" : "#E87C3C",
+        "dialogBox.button.normal" : "#DFDFDF",
+        "page.background" : "#838689",
+        "button.normal" : "#FCFCFC"
+    }
+};loadHandler&&loadHandler(22, 'q_621E5A13-D29B-48D0-BB31-D3E8F6EB3326', quizInfo, skinSettings);})();
